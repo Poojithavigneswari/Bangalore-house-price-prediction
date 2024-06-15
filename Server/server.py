e1858c6f4a445d7907925fa9af897ec34ba9530e
Mangapuram Poojitha Vigneswari <155978969+Poojithavigneswari@users.noreply.github.com>
2024-06-15 23:58:07 +0530

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import util
+import os
 
 
 app = Flask(__name__)
@@ -32,5 +33,5 @@ def predict_home_price():
 
 if __name__ == "__main__":
     print("Starting Python Flask Server...")
-    util.load_saved_artifacts()
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
